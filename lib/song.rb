c0985eb55353bac5b053210c8740cc2b5b41121e
YOUR_GITHUB_USER_NAME <YOUR_GITHUB_EMAIL_ADDRESS>
2021-01-21 23:32:14 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+class Song
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        @@all.clear
+    end
+
+    def self.create(name)
+        new_song = self.new(name)
+        new_song
+
+    end
+
+
+
+
+end
